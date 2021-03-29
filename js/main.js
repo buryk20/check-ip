d62b4b221da8c590b23910ca49319226b2ee003f
buryk20 <73736907+buryk20@users.noreply.github.com>
2021-03-29 21:54:27 +0300

    js

--- js/main.js
+++ js/main.js
@@ -1,11 +1,12 @@
 function show_hide_password(target) {
     var input = document.getElementById('password-input');
     if (input.getAttribute('type') == 'password') {
-        target.classList.remove('check-input-pas-icon');
+        target.classList.add('view');
         input.setAttribute('type', 'text');
     } else {
-        target.classList.add('check-input-pas-icon-actively');
+        target.classList.remove('view');
         input.setAttribute('type', 'password');
     }
     return false;
 }
+
